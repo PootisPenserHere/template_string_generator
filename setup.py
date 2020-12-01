6dd67a409d9d660105f3c12713a67f727f96678f
Jose Pablo Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2020-11-30 19:35:56 -0700

    [mod] Explicit package path

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/PootisPenserHere/template_string_generator",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('template_string_generator/__init__.py'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3",
