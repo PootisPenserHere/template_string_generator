0fc5d4d98e1fe21ebe6f43737e4d576b0aa63f7a
Jose Pablo Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2020-11-30 19:50:57 -0700

    [mod] Moving the code to a namespace

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="template_string_generator",
-    version="0.0.2",
+    version="0.0.3",
     author="Jose Pablo Aramburo",
     author_email="josepablo.aramburo@laziness.rocks",
     description="Generates all possible combinations of a string based on a given template",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/PootisPenserHere/template_string_generator",
-    packages=setuptools.find_packages('template_string_generator/__init__.py'),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3",
